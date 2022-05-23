55050570e2b71771d7b60993ceeab545ceb78d78
TerraBoii <kostyaborisenko05@gmail.com>
2022-05-23 19:38:45 +0300

    Code formatted Added icon ignore for Linux systems Removed "Button resize" code Renamed unused variables

--- app_code.py
+++ app_code.py
@@ -1,119 +1,128 @@
-from tkinter import Tk, Frame, Label, Button, Entry, Grid, Toplevel
-from random import randint
-from webbrowser import open_new_tab
+from tkinter import Tk, Frame, Label, Button, Entry, Grid
 from configparser import ConfigParser
+from webbrowser import open_new_tab
+from platform import system
+from random import randint
 
 # Color section
 parser = ConfigParser()
 
 parser.read("theme_colors.txt")
-current_theme = parser.get('theme', "current_theme")
 bg = parser.get("colors", "background")
-fg = parser.get("colors", "foreground")
-active_fg = parser.get("colors", "active_foreground")
+num_fg = parser.get("colors", "num_btn_fore")
 home_btn_fg = parser.get("colors", "home_btn_fore")
+active_fg = parser.get("colors", "active_foreground")
 home_btn_active_fg = parser.get("colors", "home_bts_active_fore")
+num_active_fg = parser.get("colors", "num_btn_active_fore")
+current_theme = parser.get('theme', "current_theme")
 main_btn_bg = parser.get("colors", "main_btn_back")
 num_bg = parser.get("colors", "num_btn_back")
-num_fg = parser.get("colors", "num_btn_fore")
-num_active_fg = parser.get("colors", "num_btn_active_fore")
+fg = parser.get("colors", "foreground")
 
 
 def neon_theme_colors():
+
     global parser
     parser.read("theme_colors.txt")
     parser.set('theme', "current_theme", "neon")
     parser.set("colors", "background", "#000000")
     parser.set("colors", "foreground", "#00ff00")
-    parser.set("colors", "active_foreground", "#008000")
-    parser.set("colors", "home_btn_fore", "#0000FF")
-    parser.set("colors", "home_bts_active_fore", "#00008B")
-    parser.set("colors", "main_btn_back", "#000000")
-    parser.set("colors", "num_btn_back", "#0a0a0a")
     parser.set("colors", "num_btn_fore", "#00ff00")
+    parser.set("colors", "num_btn_back", "#0a0a0a")
+    parser.set("colors", "home_btn_fore", "#0000FF")
+    parser.set("colors", "main_btn_back", "#000000")
+    parser.set("colors", "active_foreground", "#008000")
+    parser.set("colors", "home_bts_active_fore", "#00008B")
     parser.set("colors", "num_btn_active_fore", "#008000")
-    with open("theme_colors.txt", "w") as configfile:
-        parser.write(configfile)
+    with open("theme_colors.txt", "w") as config_file:
+        parser.write(config_file)
+
     # Set colors
     parser.read("colors.txt")
     global bg, fg, active_fg, home_btn_fg, home_btn_active_fg, main_btn_bg, num_bg, num_fg, num_active_fg, current_theme
-    current_theme = parser.get('theme', "current_theme")
-    bg = parser.get("colors", "background")
-    fg = parser.get("colors", "foreground")
-    active_fg = parser.get("colors", "active_foreground")
-    home_btn_fg = parser.get("colors", "home_btn_fore")
     home_btn_active_fg = parser.get("colors", "home_bts_active_fore")
-    main_btn_bg = parser.get("colors", "main_btn_back")
-    num_bg = parser.get("colors", "num_btn_back")
-    num_fg = parser.get("colors", "num_btn_fore")
     num_active_fg = parser.get("colors", "num_btn_active_fore")
+    active_fg = parser.get("colors", "active_foreground")
+    current_theme = parser.get('theme', "current_theme")
+    main_btn_bg = parser.get("colors", "main_btn_back")
+    home_btn_fg = parser.get("colors", "home_btn_fore")
+    num_fg = parser.get("colors", "num_btn_fore")
+    num_bg = parser.get("colors", "num_btn_back")
+    fg = parser.get("colors", "foreground")
+    bg = parser.get("colors", "background")
 
 
 def dark_theme_colors():
+
     global parser
     parser.read("theme_colors.txt")
     parser.set("theme", "current_theme", "dark")
     parser.set("colors", "background", "#000000")
     parser.set("colors", "foreground", "#ffffff")
-    parser.set("colors", "active_foreground", "#5e5e5e")
-    parser.set("colors", "home_btn_fore", "#474747")
-    parser.set("colors", "home_bts_active_fore", "#333333")
-    parser.set("colors", "main_btn_back", "#000000")
     parser.set("colors", "num_btn_back", "#0a0a0a")
     parser.set("colors", "num_btn_fore", "#8c8c8c")
+    parser.set("colors", "home_btn_fore", "#474747")
+    parser.set("colors", "main_btn_back", "#000000")
+    parser.set("colors", "active_foreground", "#5e5e5e")
     parser.set("colors", "num_btn_active_fore", "#5e5e5e")
-    with open("theme_colors.txt", 'w') as configfile:
-        parser.write(configfile)
+    parser.set("colors", "home_bts_active_fore", "#333333")
+    with open("theme_colors.txt", 'w') as config_file:
+        parser.write(config_file)
+
     # Set colors
     parser.read("colors.txt")
     global bg, fg, active_fg, home_btn_fg, home_btn_active_fg, main_btn_bg, num_bg, num_fg, num_active_fg, current_theme
-    current_theme = parser.get('theme', "current_theme")
-    bg = parser.get("colors", "background")
-    fg = parser.get("colors", "foreground")
-    active_fg = parser.get("colors", "active_foreground")
-    home_btn_fg = parser.get("colors", "home_btn_fore")
     home_btn_active_fg = parser.get("colors", "home_bts_active_fore")
+    num_active_fg = parser.get("colors", "num_btn_active_fore")
+    active_fg = parser.get("colors", "active_foreground")
+    current_theme = parser.get('theme', "current_theme")
+    home_btn_fg = parser.get("colors", "home_btn_fore")
     main_btn_bg = parser.get("colors", "main_btn_back")
     num_bg = parser.get("colors", "num_btn_back")
     num_fg = parser.get("colors", "num_btn_fore")
-    num_active_fg = parser.get("colors", "num_btn_active_fore")
+    fg = parser.get("colors", "foreground")
+    bg = parser.get("colors", "background")
 
 
 def light_theme_colors():
+
     global parser
     parser.read("theme_colors.txt")
     parser.set('theme', "current_theme", "light")
     parser.set("colors", "background", "#bababa")
     parser.set("colors", "foreground", "#000000")
-    parser.set("colors", "active_foreground", "#000000")
-    parser.set("colors", "home_btn_fore", "#404040")
-    parser.set("colors", "home_bts_active_fore", "#5e5e5e")
-    parser.set("colors", "main_btn_back", "#292929")
-    parser.set("colors", "num_btn_back", "#999999")
     parser.set("colors", "num_btn_fore", "#4d4d4d")
+    parser.set("colors", "num_btn_back", "#999999")
+    parser.set("colors", "home_btn_fore", "#404040")
+    parser.set("colors", "main_btn_back", "#292929")
+    parser.set("colors", "active_foreground", "#000000")
     parser.set("colors", "num_btn_active_fore", "#787878")
-    with open("theme_colors.txt", 'w') as configfile:
-        parser.write(configfile)
+    parser.set("colors", "home_bts_active_fore", "#5e5e5e")
+    with open("theme_colors.txt", 'w') as config_file:
+        parser.write(config_file)
+
     # Set colors
     parser.read("colors.txt")
     global bg, fg, active_fg, home_btn_fg, home_btn_active_fg, main_btn_bg, num_bg, num_fg, num_active_fg, current_theme
-    current_theme = parser.get('theme', "current_theme")
-    bg = parser.get("colors", "background")
-    fg = parser.get("colors", "foreground")
-    active_fg = parser.get("colors", "active_foreground")
-    home_btn_fg = parser.get("colors", "home_btn_fore")
     home_btn_active_fg = parser.get("colors", "home_bts_active_fore")
+    num_active_fg = parser.get("colors", "num_btn_active_fore")
+    active_fg = parser.get("colors", "active_foreground")
+    current_theme = parser.get('theme', "current_theme")
+    home_btn_fg = parser.get("colors", "home_btn_fore")
     main_btn_bg = parser.get("colors", "main_btn_back")
     num_bg = parser.get("colors", "num_btn_back")
     num_fg = parser.get("colors", "num_btn_fore")
-    num_active_fg = parser.get("colors", "num_btn_active_fore")
+    fg = parser.get("colors", "foreground")
+    bg = parser.get("colors", "background")
 
 
 class MainAppBody(Tk):
+
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
         self.title("Guess the square root of random number")
-        self.iconbitmap("icon.ico")
+        if system().lower() != "linux":
+            self.iconbitmap("icon.ico")
         self.geometry(f"{(self.winfo_screenwidth() // 2) + 500}x{(self.winfo_screenheight() // 2) + 200}")
         self.minsize(800, 600)
         self.maxsize(self.winfo_screenwidth(), self.winfo_screenheight())
@@ -143,6 +152,7 @@ class MainAppBody(Tk):
 
 
 class GreetingsPage(Frame):
+
     def __init__(self, parent, controller):
         Frame.__init__(self, parent, bg=bg)
         self.controller = controller
@@ -156,12 +166,12 @@ class GreetingsPage(Frame):
         self.welcome_text.pack(fill='both', expand=True)
 
         self.start_button = Button(self, text="Start", bg=num_bg, fg=fg, font=("Arial", 45),
-                                   activeforeground=active_fg, activebackground=num_bg, bd=0,
+                                   activeforeground=active_fg, activebackground=num_bg, bd=0, highlightthickness=0,
                                    disabledforeground=num_bg, command=lambda: controller.show_frame(MainPage))
         self.start_button.pack(fill='both', pady=2, expand=True)
 
         self.settings_button = Button(self, text="Settings", bg=num_bg, fg=home_btn_fg, font=("Arial", 45),
-                                      activeforeground=home_btn_active_fg, activebackground=num_bg, bd=0,
+                                      activeforeground=home_btn_active_fg, activebackground=num_bg, bd=0, highlightthickness=0,
                                       disabledforeground=num_bg, command=lambda: controller.show_frame(SettingsPage))
         self.settings_button.pack(fill='both', side='bottom', expand=True)
 
@@ -184,28 +194,6 @@ class GreetingsPage(Frame):
                 self.welcome_text.config(font=('Verdana', 45))
                 self.settings_button.config(font=('Arial', 45))
                 self.start_button.config(font=('Arial', 45))
-            # Buttons resize
-            if e.height <= 610 and e.width > 1160:
-                self.start_button.config(font=('Arial', 35))
-                self.settings_button.config(font=('Arial', 35))
-            elif 610 < e.height <= 645 and e.width > 1160:
-                self.start_button.config(font=('Arial', 45))
-                self.settings_button.config(font=('Arial', 45))
-            elif 645 < e.height <= 700 and e.width > 1160:
-                self.start_button.config(font=('Arial', 55))
-                self.settings_button.config(font=('Arial', 55))
-            elif 700 < e.height <= 740 and e.width > 1160:
-                self.start_button.config(font=('Arial', 65))
-                self.settings_button.config(font=('Arial', 65))
-            elif 740 < e.height <= 795 and e.width > 1160:
-                self.start_button.config(font=('Arial', 75))
-                self.settings_button.config(font=('Arial', 75))
-            elif 795 < e.height <= 807 and e.width > 1160:
-                self.start_button.config(font=('Arial', 85))
-                self.settings_button.config(font=('Arial', 85))
-            elif e.height > 807 and e.width > 1160:
-                self.start_button.config(font=('Arial', 95))
-                self.settings_button.config(font=('Arial', 95))
 
         self.bind("<Configure>", font_resize)
 
@@ -219,18 +207,19 @@ class GreetingsPage(Frame):
 
 
 class SettingsPage(Frame):
+
     def __init__(self, parent, controller):
         Frame.__init__(self, parent, bg=bg)
         self.controller = controller
 
 
-        def call_link(*args):
+        def call_link(*_):
             open_new_tab("https://github.com/TerraBoii")
 
-        def enter(event):
+        def enter(_):
             self.created_by.config(font=("Tahoma", 15, "underline"))
 
-        def leave(event):
+        def leave(_):
             self.created_by.config(font=("Tahoma", 15))
 
         self.created_by= Label(self, bg=bg, fg="#213c91", text="Created by: TerraBoii", 
@@ -252,37 +241,34 @@ class SettingsPage(Frame):
         Grid.columnconfigure(self.themes_changers_container, 2, weight=1)
 
         self.neon_theme_btn = Button(self.themes_changers_container, text="Neon green", bg=num_bg, fg=num_fg,
-                                     font=("Times New Roman", 55),
-                                     activeforeground=num_active_fg, activebackground=num_bg, bd=0,
-                                     disabledforeground=num_bg,
-                                     command=self.change_theme_to_neon)
+                                     font=("Times New Roman", 55), highlightthickness=0, bd=0,
+                                     activeforeground=num_active_fg, activebackground=num_bg,
+                                     disabledforeground=num_bg, command=self.change_theme_to_neon)
         self.neon_theme_btn.grid(row=0, column=1, sticky='nsew')
 
-        self.dark_theme_btn = Button(self.themes_changers_container, text="Dark", bg=bg, fg=fg,
-                                     font=("Times New Roman", 55),
-                                     activeforeground=active_fg, activebackground=bg, bd=0, disabledforeground=bg,
-                                     command=self.change_theme_to_dark)
+        self.dark_theme_btn = Button(self.themes_changers_container, text="Dark", bg=bg, fg=fg, font=("Times New Roman", 55),
+                                     highlightthickness=0, bd=0, activeforeground=active_fg, activebackground=bg,
+                                     disabledforeground=bg, command=self.change_theme_to_dark)
         self.dark_theme_btn.grid(row=0, column=0, sticky='nsew')
 
-        self.light_theme_btn = Button(self.themes_changers_container, text="light", bg=bg, fg=fg,
-                                      font=("Times New Roman", 55),
-                                      activeforeground=active_fg, activebackground=bg, bd=0, disabledforeground=bg,
-                                      command=self.change_theme_to_light)
+        self.light_theme_btn = Button(self.themes_changers_container, text="light", bg=bg, fg=fg, bd=0,
+                                      font=("Times New Roman", 55), highlightthickness=0, activebackground=bg,
+                                      activeforeground=active_fg, disabledforeground=bg, command=self.change_theme_to_light)
         self.light_theme_btn.grid(row=0, column=2, sticky='nsew')
 
-        self.home_button = Button(self, text="Home", bg=num_bg, fg=home_btn_fg, font=("Arial", 45),
+        self.home_button = Button(self, text="Home", bg=num_bg, fg=home_btn_fg, font=("Arial", 45), highlightthickness=0,
                                   activeforeground=home_btn_active_fg, activebackground=num_bg, bd=0,
                                   disabledforeground=num_bg, command=lambda: controller.show_frame(GreetingsPage))
         self.home_button.pack(fill='both', side='bottom', expand=True)
 
         if current_theme == "neon":
             self.dark_theme_btn.config(state='normal')
-            self.neon_theme_btn.config(state='disabled')
             self.light_theme_btn.config(state='normal')
+            self.neon_theme_btn.config(state='disabled')
         elif current_theme == 'dark':
             self.dark_theme_btn.config(state='disabled')
-            self.neon_theme_btn.config(state='normal')
             self.light_theme_btn.config(state='normal')
+            self.neon_theme_btn.config(state='normal')
         elif current_theme == 'light':
             self.dark_theme_btn.config(state='normal')
             self.neon_theme_btn.config(state='normal')
@@ -292,17 +278,17 @@ class SettingsPage(Frame):
             """Resizes font based on window height and width"""
             # Change font for big buttons
             if e.width <= 912:
-                self.home_button.config(font=('Arial', 45))
+                # self.home_button.config(font=('Arial', 45))
                 self.neon_theme_btn.config(font=('Times New Roman', 42))
                 self.dark_theme_btn.config(font=('Times New Roman', 42))
                 self.light_theme_btn.config(font=('Times New Roman', 42))
             elif 912 < e.width <= 1160:
-                self.home_button.config(font=('Arial', 55))
+                # self.home_button.config(font=('Arial', 55))
                 self.neon_theme_btn.config(font=('Times New Roman', 50))
                 self.dark_theme_btn.config(font=('Times New Roman', 50))
                 self.light_theme_btn.config(font=('Times New Roman', 50))
             elif e.width > 1160:
-                self.home_button.config(font=('Arial', 55))
+                # self.home_button.config(font=('Arial', 55))
                 self.neon_theme_btn.config(font=('Times New Roman', 55))
                 self.dark_theme_btn.config(font=('Times New Roman', 55))
                 self.light_theme_btn.config(font=('Times New Roman', 55))
@@ -327,10 +313,9 @@ class SettingsPage(Frame):
         self.neon_theme_btn.config(state='normal')
         self.light_theme_btn.config(state='normal')
         dark_theme_colors()
+        self.settings_page_theme_update()
         page = self.controller.get_page(MainPage)
         page.main_page_theme_update()
-        page = self.controller.get_page(SettingsPage)
-        page.settings_page_theme_update()
         page = self.controller.get_page(GreetingsPage)
         page.greetings_page_theme_update()
 
@@ -339,10 +324,9 @@ class SettingsPage(Frame):
         self.neon_theme_btn.config(state='disabled')
         self.light_theme_btn.config(state='normal')
         neon_theme_colors()
+        self.settings_page_theme_update()
         page = self.controller.get_page(MainPage)
         page.main_page_theme_update()
-        page = self.controller.get_page(SettingsPage)
-        page.settings_page_theme_update()
         page = self.controller.get_page(GreetingsPage)
         page.greetings_page_theme_update()
 
@@ -351,10 +335,9 @@ class SettingsPage(Frame):
         self.neon_theme_btn.config(state='normal')
         self.light_theme_btn.config(state='disabled')
         light_theme_colors()
+        self.settings_page_theme_update()
         page = self.controller.get_page(MainPage)
         page.main_page_theme_update()
-        page = self.controller.get_page(SettingsPage)
-        page.settings_page_theme_update()
         page = self.controller.get_page(GreetingsPage)
         page.greetings_page_theme_update()
 
@@ -671,6 +654,7 @@ class MainPage(Frame):
                 self.zero.config(font=('Arial', btn_h))
                 self.clear_button.config(font=('Arial', btn_h))
                 self.confirm_button.config(font=('Arial', btn_h))
+
             # Main text resize
             if e.width <= 830:
                 txt_ssf = 51  # Main text smallest font
@@ -720,5 +704,4 @@ class MainPage(Frame):
 
 
 if __name__ == '__main__':
-    app = MainAppBody()
-    app.mainloop()
+    MainAppBody().mainloop()
